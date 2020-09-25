33770d621fe6a87791ecfe7f54e2c0314e84aa94
David Wan <davidwan58@gmail.com>
2020-09-25 20:39:06 +0800

    init upload

---
+++ README.md
@@ -0,0 +1,7 @@
+### How to effectively solve the keyboard interruption by multi-threading and notify all sub-threads in time to handle the correct end of the program ?
+
+> 多執行緒如何有效解決當鍵盤中斷時，能適時通知所有的子線程，以處理正確的結束程序 ?
+> 
+> Python 最好玩的地方就是，它其實提供了許多解決辦法來處理你的問題及你想要的結果。前提是，整合各知識並善用他們，是 python coder 必須要搞定的事情。
+
+
---
+++ threading_kbint_sample.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+__author__ = 'Wan Pei Chih <davidwan58@gmail.com>'
+
+from queue import Queue
+from threading import Thread
+from time import sleep
+import sys, os
+
+q1 = Queue()    # th1 專用
+q2 = Queue()    # th2 專用
+
+def world(outq):
+    while True:
+        if outq.qsize() > 0:
+            data = outq.get()
+            print('Get the queue: %s' %data)
+            if data == 'QUIT':
+                sys.exit('world will be exit...')
+        print('world checking...')
+        sleep(1.5)
+
+def hello(outq):
+    while True:
+        if outq.qsize() > 0:
+            data = outq.get()
+            print('Get the queue: %s' %data)
+            if data == 'QUIT':
+                sys.exit('hello will be exit...')
+        print('hello checking...')
+        sleep(1)
+
+
+if __name__ == '__main__':
+    th1 = Thread(target=hello, args=(q1,))
+    th2 = Thread(target=world, args=(q2,))
+    th1.start()
+    th2.start()
+    while True:
+        try:
+            sleep(1)
+            if not th1.is_alive() and not th2.is_alive():
+                print('Child threading all exited.')
+                break
+        except KeyboardInterrupt:
+            q1.put('QUIT')
+            q2.put('QUIT')
+            th1.join()
+            th2.join()
+    sys.exit('main thead has be exit')
+
+
